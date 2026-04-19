d29a5b4a4abf047067eb26e69d5365b104a0e071
Yevgenii Vasylenko <yevgenii.vasyenko@gmail.com>
2026-04-19 11:09:24 +0300

    secur appsetigs

--- HL_C_Pro/HL_C_Pro_14/Models/PostgresContext.cs
+++ HL_C_Pro/HL_C_Pro_14/Models/PostgresContext.cs
@@ -20,8 +20,15 @@ public partial class PostgresContext : DbContext
     public virtual DbSet<Category> Categories { get; set; }
 
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see https://go.microsoft.com/fwlink/?LinkId=723263.
-        => optionsBuilder.UseNpgsql("Host=localhost;Port=5432;Database=postgres;Username=postgres;Password=postgres;");
+    {
+        if (!optionsBuilder.IsConfigured)
+        {
+            var config = new Microsoft.Extensions.Configuration.ConfigurationBuilder()
+                .AddJsonFile("appsettings.json")
+                .Build();
+            optionsBuilder.UseNpgsql(config.GetConnectionString("DefaultConnection"));
+        }
+    }
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
