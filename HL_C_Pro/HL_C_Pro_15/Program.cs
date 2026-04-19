4af1fdda1e578fdee03ffef782d50e93295c3ed3
Yevgenii Vasylenko <yevgenii.vasyenko@gmail.com>
2026-04-19 12:52:52 +0300

    dz 15 test

---
+++ HL_C_Pro/HL_C_Pro_15/Program.cs
@@ -0,0 +1,35 @@
+﻿var exchange = new FakeExchangeService();
+var res = new TradingBot(exchange).ExecuteStrategy("BTCUSD", 1000);
+Console.WriteLine(res);
+
+public class FakeExchangeService : IExchangeService
+{
+    private static readonly Random _rng = new();
+    public decimal GetCurrentPrice(string symbol) =>
+        _rng.Next(800, 1201);
+}
+
+public interface IExchangeService
+{
+    decimal GetCurrentPrice(string symbol);
+}
+
+public class TradingBot
+{
+    private readonly IExchangeService _exchange;
+
+    public TradingBot(IExchangeService exchange)
+    {
+        _exchange = exchange;
+    }
+
+    public string ExecuteStrategy(string symbol, decimal averagePrice)
+    {
+        var currentPrice = _exchange.GetCurrentPrice(symbol);
+
+        if (currentPrice <= averagePrice * 0.9m) return "Buy";
+        if (currentPrice >= averagePrice * 1.1m) return "Sell";
+
+        return "Hold";
+    }
+}
